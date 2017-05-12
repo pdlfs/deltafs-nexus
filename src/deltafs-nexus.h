1a5f0ec0b5cbd9a471c6b2436f4ac62d24fd5147
George Amvrosiadis <gamvrosi@cs.cmu.edu>
2017-05-12 02:21:46 -0600

    Add header stub

---
+++ src/deltafs-nexus.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2016-2017 Carnegie Mellon University.
+ *
+ * All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file. See the AUTHORS file for names of contributors.
+ */
+
+#pragma once
